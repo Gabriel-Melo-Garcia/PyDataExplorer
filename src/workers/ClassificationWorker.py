19c2520f950acb3ceeefb104e3fbc3d83394bda6
hendgarcia <gabrielmelogarcia.ie@gmail.com>
2025-03-18 13:51:25 +0000

    classification worker

--- src/workers/ClassificationWorker.py
+++ src/workers/ClassificationWorker.py
@@ -5,15 +5,14 @@ from PyQt6.QtWidgets import QFileDialog
 class ClassificationWorker(QThread):
     finished = pyqtSignal(bool, str, dict)
     progress = pyqtSignal(str)
-
+  
     def __init__(self, model, features, target, model_name):
         super().__init__()
         self.model = model
         self.features = features
         self.target = target
         self.model_name = model_name
-
+  
     def run(self):
         success, message, results = self.model.train_classification_models(self.features, self.target, self.model_name)
         self.finished.emit(success, message, results)
-        
